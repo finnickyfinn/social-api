9cd5a1854054775c457972306727f41f710de82d
finnickyfinn <finncastell04@gmail.com>
2023-08-30 23:46:21 -0400

    added code

--- utils/seed.js
+++ utils/seed.js
@@ -1,13 +1,11 @@
 const connection = require('../config/connection');
 const { User } = require('../models');
 const { userRandomizer } = require('./data');
-// const { userRandomizer, thoughtRandomizer, reactionRandomizer } = require('./data'); --> Possible future dev
 
 connection.on('error', (err) => err);
 
 connection.once('open', async () => {
   console.log('connected');
-  // Delete the collections if they exist
   let thoughtsDB = await connection.db.listCollections({ name: 'thoughts' }).toArray();
   if (thoughtsDB.length) {
     await connection.dropCollection('thoughts');
